d41af547d5341f4d3e7789060b041bea7bccfeb0
Kavicente <vncbcstll@gmail.com>
2025-09-22 01:13:03 +0800

    Update

--- PNPDashboard.py
+++ PNPDashboard.py
@@ -1,6 +1,9 @@
 from alert_data import alerts
 from collections import Counter
 import logging
+import sqlite3
+import os
+
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +23,12 @@ def get_latest_alert():
     except Exception as e:
         logger.error(f"Error in get_latest_alert: {e}")
         return None
+    
+def get_heatmap_data(municipality):
+    db_path = os.path.join(os.path.dirname(__file__), '..', 'database', 'users_web.db')
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT lat, lon FROM pnp_response WHERE municipality = ?', (municipality,))
+    data = cursor.fetchall()
+    conn.close()
+    return [{'lat': row[0], 'lon': row[1]} for row in data]
