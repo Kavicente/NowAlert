91beec36a242c4d4948c05254af25010cca80d7c
Kavicente <vncbcstll@gmail.com>
2025-10-15 02:23:52 +0800

    Update

--- SignUpType.py
+++ SignUpType.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, send_file, url_for
+import os
 
 def signup_type():
     return render_template('SignUpType.html')
@@ -8,3 +9,10 @@ def login_type():
 
 def choose_login_type():
     return render_template('LoginType.html')
+
+def download_apk_folder():
+    zip_path = os.path.join(os.path.dirname(__file__), 'static', 'AlertNow.zip')
+    if os.path.exists(zip_path):
+        return send_file(zip_path, as_attachment=True, download_name='AlertNow.zip')
+    else:
+        return "File not found", 404
