8f47c5393eeb2ac963277d187dbc865b10c4be7a
Kavicente <vncbcstll@gmail.com>
2025-10-21 22:24:58 +0800

    Multiple Updates

--- CDRRMODashboard.py
+++ CDRRMODashboard.py
@@ -13,7 +13,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-def get_latest_alert():
+def get_new_alert():
     if alerts:
         return list(alerts)[-1]
     return None
