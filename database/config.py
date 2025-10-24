98f737cff0fa12f8462566bc86dc47993d699570
Kavicente <vncbcstll@gmail.com>
2025-10-24 18:37:05 +0800

    Multiple Updates

--- database/config.py
+++
@@ -1,7 +0,0 @@
-DATABASE_CONFIG = {
-    'dbname': 'dashboards',
-    'user': 'postgres',
-    'password': 'vnccstll162223',  # Replace with your PostgreSQL password
-    'host': 'localhost',
-    'port': '5432'
-}
