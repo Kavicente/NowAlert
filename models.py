13ad317fd72f6ce91d9fec6791c9b7c5d848c26d
Kavicente <vncbcstll@gmail.com>
2025-12-01 23:46:37 +0800

    Changed Model Random Forest

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 # Load road accident model
 road_accident_predictor = None
 try:
-    road_accident_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Road Models', 'road_predictor_lr.pkl'))
+    road_accident_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Road Models', 'road_predictor_rf.pkl'))
     logger.info("road_accident_predictor_lr.pkl loaded successfully.")
 except FileNotFoundError:
     logger.error("road_accident_predictor_lr.pkl not found.")
@@ -16,7 +16,7 @@ except Exception as e:
     
 fire_accident_predictor = None
 try:
-    fire_accident_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Fire Models', 'fire_predictor_lr.pkl'))
+    fire_accident_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Fire Models', 'fire_predictor_rf.pkl'))
     logger.info("fire_predictor_lr.pkl loaded successfully.")
 except FileNotFoundError:
     logger.error("fire_predictor_lr.pkl not found.")
@@ -25,7 +25,7 @@ except Exception as e:
 
 crime_predictor = None
 try:
-    crime_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Crime Models', 'crime_predictor_lr.pkl'))
+    crime_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Crime Models', 'crime_predictor_rf.pkl'))
     logger.info("crime_predictor_lr.pkl loaded successfully.")
 except FileNotFoundError:
     logger.error("crime_predictor_lr.pkl not found.")
@@ -35,7 +35,7 @@ except Exception as e:
 
 health_predictor = None
 try:
-    health_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Health Models', 'health_predictor_lr.pkl'))
+    health_predictor = joblib.load(os.path.join(os.path.dirname(__file__), 'training', 'Health Models', 'health_predictor_rf.pkl'))
     logger.info("health_predictor_lr.pkl loaded successfully.")
 except FileNotFoundError:
     logger.error("health_predictor_lr.pkl not found.")
